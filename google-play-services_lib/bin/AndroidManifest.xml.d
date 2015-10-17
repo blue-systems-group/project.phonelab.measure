952f92e6be99ac6c65bf1dc6b1f5b1ed3b913bda
Jinghao Shi <jinghaos@buffalo.edu>
2015-10-17 18:46:13 -0400

    fix gitignore

--- google-play-services_lib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/jinghaos/projects/allyoucanmeasure/google-play-services_lib/bin/AndroidManifest.xml : \
-/home/jinghaos/projects/allyoucanmeasure/google-play-services_lib/AndroidManifest.xml \
