91970319914213995b723246cc278b9b9ec9c550
Jinghao Shi <jinghaos@buffalo.edu>
2015-10-17 18:33:11 -0400

    add google play api

---
+++ google-play-services_lib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/jinghaos/projects/allyoucanmeasure/google-play-services_lib/bin/AndroidManifest.xml : \
+/home/jinghaos/projects/allyoucanmeasure/google-play-services_lib/AndroidManifest.xml \
